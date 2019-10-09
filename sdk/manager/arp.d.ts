9890e2ce9c3fd45cb387ff5ab2304381392c4136
Tjalling Tolle <tjallingt@gmail.com>
2019-10-09 12:10:38 +0200

    fix typings

--- sdk/manager/arp.d.ts
+++ sdk/manager/arp.d.ts
@@ -1,19 +1,16 @@
+import Manager = require("../lib/Manager");
+
 export = ManagerArp;
-declare const ManagerArp_base: any;
 /**
  * @memberof Homey
  * @namespace ManagerArp
  * @global
  */
-declare class ManagerArp extends ManagerArp_base {
-    [x: string]: any;
+declare class ManagerArp extends Manager {
     /**
      * Get an ip's MAC address
      * @param {string} ip
-     * @param {Function} callback
-     * @param {Error} callback.err
-     * @param {string} callback.mac
-     * @returns Promise
+     * @returns {Promise<string>}
      */
-    getMAC(ip: string, callback: Function): any;
+    getMAC(ip: string): Promise<string>;
 }
