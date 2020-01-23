84b6dd773d9dd5b6529c908d0537e794be6b4569
Kyle Athom <kyle@athom.nl>
2020-01-23 11:28:39 +0100

    Moved athom-cli commands into node-homey

--- bin/homey.js
+++ bin/homey.js
@@ -1,3 +1,24 @@
 #!/usr/bin/env node
 
-require('athom-cli/bin/athom-cli.js');
+'use strict';
+
+const pkg = require('../package.json');
+const yargs = require('yargs');
+const updateNotifier = require('update-notifier');
+const AthomMessage = require('..').AthomMessage;
+
+(async () => {
+
+	await AthomMessage.notify();
+	updateNotifier({ pkg }).notify({
+		isGlobal: true,
+	});
+
+	yargs
+		.commandDir('./cmds')
+		.demandCommand()
+		.strict()
+		.help()
+		.argv;
+
+})();
