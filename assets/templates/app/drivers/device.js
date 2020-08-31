e37fbc7a31765a0b30d672d84d7e2ce0f4adc010
Tjalling Tolle <tjallingt@gmail.com>
2020-08-31 14:50:25 +0200

    feat: update templates for sdk v3

--- assets/templates/app/drivers/device.js
+++ assets/templates/app/drivers/device.js
@@ -3,11 +3,47 @@
 const Homey = require('homey');
 
 class MyDevice extends Homey.Device {
-	
-	onInit() {
-		this.log('MyDevice has been inited');
-	}
-	
+  /**
+   * onInit is called when the device is initialized.
+   */
+  async onInit() {
+    this.log('MyDevice has been initialized');
+  }
+
+  /**
+   * onAdded is called when the user adds the device, called just after pairing.
+   */
+  onAdded() {
+    this.log('MyDevice has been added');
+  }
+
+  /**
+   * onSettings is called when the user updates the device's settings.
+   * @param {object} event the onSettings event data
+   * @param {object} event.oldSettings The old settings object
+   * @param {object} event.newSettings The new settings object
+   * @param {string[]} event.changedKeys An array of keys changed since the previous version
+   * @returns {Promise<string|void>} return a custom message that will be displayed
+   */
+  async onSettings({ oldSettings, newSettings, changedKeys }) {
+    this.log('MyDevice settings where changed');
+  }
+
+  /**
+   * onRenamed is called when the user updates the device's name.
+   * This method can be used this to synchronize the name to the device or bridge.
+   * @param {string} name The new name
+   */
+  onRenamed(name) {
+    this.log('MyDevice was renamed');
+  }
+
+  /**
+   * onDeleted is called when the user deleted the device.
+   */
+  onDeleted() {
+    this.log('MyDevice has been deleted');
+  }
 }
 
 module.exports = MyDevice;
