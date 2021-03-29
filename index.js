aab05078cf11168d859261ecaae8667610310039
tjallingt <tjallingt@gmail.com>
2021-03-29 10:47:27 +0200

    chore: remove ledring commands

--- index.js
+++ index.js
@@ -11,4 +11,3 @@ module.exports.Settings = new (require('./lib/Settings'))();
 module.exports.AthomApi = new (require('./lib/AthomApi'))();
 module.exports.AthomMessage = new (require('./lib/AthomMessage'))();
 module.exports.App = require('./lib/App');
-module.exports.Animation = require('./lib/Animation');
