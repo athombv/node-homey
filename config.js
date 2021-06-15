5867b2303c910a43697d6ebbb18e19fe0f64e04d
Christian Roring <christian@athom.nl>
2021-06-15 12:51:34 +0200

    Added the shortcut 'homey tools' to the cli for easy acces to the developer tools

--- config.js
+++ config.js
@@ -5,4 +5,5 @@ module.exports = {
   athomApiClientSecret: '2f1cf5ad5917eac06284c9b3071406c6db318274',
   athomApiLoginUrl: 'https://cli.athom.com',
   athomMessageUrl: 'https://go.athom.com/cli-message',
+  homeyDevToolsUrl: 'https://tools.developer.homey.app',
 };
