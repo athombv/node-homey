3ca917ae03994302d3b2f73a6d92c01822929bb9
tjallingt <tjallingt@gmail.com>
2021-06-25 09:47:12 +0200

    chore: feedback

--- bin/homey.js
+++ bin/homey.js
@@ -4,28 +4,21 @@
 
 const yargs = require('yargs');
 const updateNotifier = require('update-notifier');
+const semver = require('semver');
+
 const pkg = require('../package.json');
 const { AthomMessage } = require('..');
 
-const REQUIRED_NODE_VERSION = '10.10';
+const MINIMUM_NODE_VERSION = 'v12.0.0';
 
 try {
-  let { version } = process;
-  if (version.indexOf('v') === 0) version = version.substr(1);
-  version = version.split('.');
-
-  let majorVersion = version[0];
-  majorVersion = parseInt(majorVersion, 10);
-
-  let minorVersion = version[1];
-  minorVersion = parseInt(minorVersion, 10);
-
-  if (majorVersion < 10 || (majorVersion === 10 && minorVersion < 10)) {
-    console.error(`Error: node-homey requires Node.js v${REQUIRED_NODE_VERSION} or higher to run. Please upgrade your Node.js version and try again.`);
+  if (semver.gte(process.version, MINIMUM_NODE_VERSION)) {
+    console.error(`Error: node-homey requires Node.js ${MINIMUM_NODE_VERSION} or higher to run. Please upgrade your Node.js version and try again.`);
     return;
   }
 } catch (err) {
-  console.error(`Failed to determine Node.js version, please make sure you're using version ${REQUIRED_NODE_VERSION} or higher.`);
+  console.error(`Failed to determine Node.js version, please make sure you're using version ${MINIMUM_NODE_VERSION} or higher.`);
+  return;
 }
 
 (async () => {
