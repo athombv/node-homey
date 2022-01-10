e40d3c47a115edd2bff07e4cf8c62bb148047e86
tjallingt <tjallingt@gmail.com>
2022-01-10 09:50:37 +0100

    chore: remove cli-table dependency

--- bin/cmds/list.js
+++ bin/cmds/list.js
@@ -1,6 +1,5 @@
 'use strict';
 
-const Table = require('cli-table');
 const colors = require('chalk');
 const { Log } = require('../../index');
 const { AthomApi } = require('../../index');
@@ -10,37 +9,25 @@ exports.handler = async () => {
   try {
     const homeys = await AthomApi.getHomeys();
 
-    const table = new Table({
-      head: [
-        'ID',
-        'Name',
-        'Version',
-        'API',
-        'Language',
-        'Users',
-        'Role',
-        'USB',
-      ].map(title => colors.white.bold(title)),
-    });
-
     homeys.sort((a, b) => {
       return -(a.state || '').localeCompare((b.state || ''));
     });
 
-    homeys.forEach(homey => {
-      table.push([
-        homey._id,
-        homey.name,
-        homey.softwareVersion,
-        homey.apiVersion,
-        homey.language,
-        homey.users && homey.users.length,
-        homey.role,
-        homey.usb ? 'Yes' : '-',
-      ].map(value => value || '-'));
-    });
+    const table = {};
+    for (const homey of homeys) {
+      table[homey._id] = {
+        Name: homey.name,
+        Version: homey.softwareVersion ? homey.softwareVersion : '-',
+        API: homey.apiVersion ? homey.apiVersion : '-',
+        Language: homey.language ? homey.language : '-',
+        Users: homey.users && homey.users.length,
+        Role: homey.role ? homey.role : '-',
+        USB: homey.usb ? 'Yes' : '-',
+      };
+    }
 
-    Log(table.toString());
+    // eslint-disable-next-line no-console
+    console.table(table);
   } catch (err) {
     Log(colors.red(err.message));
   }
