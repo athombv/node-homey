300750a79dffa33e66fb5d57e87f19dce421af9d
Emile Nijssen <emile@athom.nl>
2024-07-16 13:35:11 +0200

    wip

--- bin/cmds/app/translate.js
+++ bin/cmds/app/translate.js
@@ -1,5 +1,7 @@
 'use strict';
 
+const colors = require('colors');
+
 const Log = require('../../../lib/Log');
 const App = require('../../../lib/App');
 
@@ -15,6 +17,11 @@ exports.builder = yargs => {
       default: process.env.OPENAI_API_KEY,
       type: 'string',
       description: 'OpenAI API key. You can create an API Key on https://platform.openai.com/api-keys.',
+    })
+    .option('model', {
+      default: 'gpt-4o',
+      type: 'string',
+      description: 'OpenAI model to use.',
     });
 };
 exports.handler = async yargs => {
@@ -23,11 +30,16 @@ exports.handler = async yargs => {
     await app.translateWithOpenAI({
       languages: yargs.languages.split(',').map(lang => lang.trim()),
       apiKey: yargs.apiKey,
+      model: yargs.model,
     });
     await app.preprocess();
     await app.validate({
       level: yargs.level,
     });
+
+    Log('');
+    Log(colors.yellow('The app has been translated using AI, so results may vary. Please check every file manually before committing.'));
+
     process.exit(0);
   } catch (err) {
     Log.error(err);
