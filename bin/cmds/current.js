a7e8876f7bc0939026c4354b321fbfed51c0fcc5
Jeroen Wienk <jeroenwienk@gmail.com>
2026-02-21 15:11:07 +0100

    refactor(cli): migrate current command to ESM

--- bin/cmds/current.js
+++ bin/cmds/current.js
@@ -1,22 +1,8 @@
 'use strict';
 
-const Log = require('../../lib/Log');
-const AthomApi = require('../../services/AthomApi');
-
 exports.desc = 'Show the active Homey';
 exports.handler = async () => {
-  try {
-    const activeHomey = await AthomApi.getSelectedHomey();
-
-    if (!activeHomey) {
-      Log('No active Homey selected. Run `homey select` to choose one.');
-      process.exit(0);
-    }
-
-    Log(`Active Homey: ${activeHomey.name} (${activeHomey.id})`);
-    process.exit(0);
-  } catch (err) {
-    Log.error(err);
-    process.exit(1);
-  }
+  // eslint-disable-next-line node/no-unsupported-features/es-syntax, import/extensions
+  const cmd = await import('./current.mjs');
+  return cmd.default();
 };
---
+++ bin/cmds/current.mjs
@@ -0,0 +1,21 @@
+'use strict';
+
+import Log from '../../lib/Log.js';
+import AthomApi from '../../services/AthomApi.js';
+
+export default async function handler() {
+  try {
+    const activeHomey = await AthomApi.getSelectedHomey();
+
+    if (!activeHomey) {
+      Log('No active Homey selected. Run `homey select` to choose one.');
+      process.exit(0);
+    }
+
+    Log(`Active Homey: ${activeHomey.name} (${activeHomey.id})`);
+    process.exit(0);
+  } catch (err) {
+    Log.error(err);
+    process.exit(1);
+  }
+}
